79fb815749d742c4efde737135deaa78f68f7a0d
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-08-09 21:09:09 -0400

    added route prefixes and connected routes with controllers

---
+++ routes/api/pizza-routes.js
@@ -0,0 +1,25 @@
+const router = require('express').Routes();
+
+// import the controller methods
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+} = require('../../controllers/pizza-controller');
+
+// set up GET all and POST at /api/pizzas
+router
+    .route('/')
+    .get(getAllPizza)
+    .post(createPizza);
+
+// set up GET one, PUT, and DELETE at /api/pizzas/:id
+router
+    .route('/:id')
+    .get(getPizzaById)
+    .put(updatePizza)
+    .delete(deletePizza);
+
+module.exports = router;
